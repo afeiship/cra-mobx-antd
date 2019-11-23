7d2bb81e2dadc3cdc0330230945ac72ac17cdd66
afeiship <1290657123@qq.com>
2019-11-23 18:41:30 +0800

    add: mv public ejs

---
+++ build/server.js
@@ -0,0 +1,23 @@
+const server = require('express');
+const app = server();
+const portNumber = 3012;
+const sourceDir = 'dist';
+const compression = require('compression');
+const proxy = require('http-proxy-middleware');
+
+//OPEN GZIP
+app.use(compression({ level: 6 }));
+app.use(server.static(sourceDir));
+
+app.use(
+  '/api',
+  proxy({
+    target: 'http://test.demo.com',
+    changeOrigin: true
+  })
+);
+
+app.listen(portNumber, () => {
+  console.log(`Express web server started: http://localhost:${portNumber}`);
+  console.log(`Serving content from /${sourceDir}/`);
+});
