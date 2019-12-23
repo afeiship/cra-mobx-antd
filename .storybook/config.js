571877a9076919575ec544f1f94ad6c8af95cefe
afeiship <1290657123@qq.com>
2019-12-23 19:56:54 +0800

    add: webpack with spa-webpack fonfig

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
+
+import { Provider } from 'mobx-react';
+import stores from '#/stores';
+
+// mobx store/inject support(?)
+addDecorator((story) => <Provider store={stores}>{story()}</Provider>);
+addDecorator(StoryRouter());
 
 configure(require.context('../src', true, /\.story\.js$/), module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,31 +1,29 @@
 const path = require('path');
-
+const { configs, loaders, plugins } = require('@feizheng/webpack-app-kits');
+const nxFlattern = require('@feizheng/next-flatten');
 module.exports = async ({ config, mode }) => {
-  config.devtool = 'source-map';
-  config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+  // config.devtool = 'source-map';
+  config.resolve.alias = configs.alias(config.resolve.alias);
+  config.module.rules = nxFlattern([
+    config.module.rules,
+    loaders.environment(),
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    }
+  ]);
 
-  config.module.rules.find(
-    (rule) => rule.test.toString() === '/\\.css$/'
-  ).exclude = /\.module\.css$/;
-
-  config.module.rules.push({
-    test: /\.module\.css$/,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: true
-        }
-      }
-    ]
-  });
-
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: ['style-loader', 'css-loader', 'sass-loader'],
-    include: path.resolve(__dirname, '../')
-  });
+  config.plugins = nxFlattern([
+    config.plugins,
+    plugins.provide({
+      Blank: ['@feizheng/react-blank', 'default'],
+      IfElse: ['@feizheng/react-if-else', 'default'],
+      RCM: ['@feizheng/react-condition-manager', 'default'],
+      RSM: ['@feizheng/react-status-manager', 'default'],
+      cx: 'classnames'
+    })
+  ]);
 
   return config;
 };
