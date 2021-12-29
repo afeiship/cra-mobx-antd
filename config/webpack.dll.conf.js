d2ae30a6bfd0ee510028e210e0bb9f3bcb38667e
afeiship <1290657123@qq.com>
2021-12-29 17:36:10 +0800

    feat: vendor for dll

---
+++ config/webpack.dll.conf.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const dllPath = '../.tmp/libs';
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    vendors: ['react', 'antd', 'react-dom'],
+  },
+  output: {
+    path: path.join(__dirname, dllPath),
+    filename: '[name].[hash:6].dll.js',
+    // vendor.dll.js中暴露出的全局变量名
+    // 保持与 webpack.DllPlugin 中名称一致
+    library: '[name]_[hash]',
+  },
+  plugins: [
+    // 清除之前的dll文件
+    new CleanWebpackPlugin(),
+    // 设置环境变量
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    // manifest.json 描述动态链接库包含了哪些内容
+    new webpack.DllPlugin({
+      path: path.join(__dirname, dllPath, '[name]-manifest.json'),
+      // 保持与 output.library 中名称一致
+      name: '[name]_[hash]',
+      context: process.cwd(),
+    }),
+  ],
+};
--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 
 module.exports = {
   // The Webpack config to use when compiling your react app for development or production.
@@ -12,6 +14,21 @@ module.exports = {
         React: 'react',
         ReactDOM: 'react-dom',
         View: ['@jswork/styled-box', 'default'],
+      }),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      new webpack.DllReferencePlugin({
+        manifest: require('./.tmp/libs/vendors-manifest.json'),
+      }),
+      // 将 dll 注入到 生成的 html 模板中
+      new AddAssetHtmlPlugin({
+        // dll文件位置
+        filepath: path.resolve(__dirname, '.tmp/libs/*.js'),
+        // dll 引用路径
+        publicPath: './static/libs',
+        // dll最终输出的目录
+        outputPath: './static/libs',
       }),
     ],
   },
