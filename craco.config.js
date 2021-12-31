70488b95f120015bdba3158fa025d1296c33b284
afeiship <1290657123@qq.com>
2021-12-31 16:09:58 +0800

    refactor: craco

--- craco.config.js
+++ craco.config.js
@@ -1,32 +1,15 @@
-const path = require('path');
-const webpack = require('webpack');
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+const spaBase = require('@jswork/craco-plugin-spa-base');
+const dllRefs = require('@jswork/craco-plugin-dll-refs');
+const spm = require('@jswork/craco-plugin-speed-measure');
+const banner = require('@jswork/craco-plugin-banner');
+const analyzer = require('@jswork/craco-plugin-bundle-analyzer');
 
 module.exports = {
-  // The Webpack config to use when compiling your react app for development or production.
-  webpack: {
-    alias: {
-      '@': path.join(__dirname, 'src'),
-    },
-    plugins: [
-      // new BundleAnalyzerPlugin({ openAnalyzer: false }),
-      new webpack.ProvidePlugin({
-        React: 'react',
-        ReactDOM: 'react-dom',
-        View: ['@jswork/styled-box', 'default'],
-      }),
-      new webpack.DllReferencePlugin({
-        manifest: require('./src/assets/libs/vendors-manifest.json'),
-      }),
-      // 将 dll 注入到 生成的 html 模板中
-      new AddAssetHtmlPlugin({
-        // dll文件位置
-        filepath: path.resolve(__dirname, './src/assets/libs/*.js'),
-        // dll 引用路径
-        publicPath: './static/libs',
-        // dll最终输出的目录
-        outputPath: './static/libs',
-      }),
-    ],
-  },
+  plugins: [
+    { plugin: spaBase },
+    { plugin: spm },
+    { plugin: banner },
+    { plugin: analyzer },
+    { plugin: dllRefs },
+  ],
 };
