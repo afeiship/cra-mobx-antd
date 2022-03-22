5d12dcd33add3e12874e40635315d02d4f4bde67
afeiship <1290657123@qq.com>
2022-03-22 21:41:12 +0800

    feat: add spm debug test

--- craco.config.js
+++ craco.config.js
@@ -4,15 +4,18 @@ const spm = require('@jswork/craco-plugin-speed-measure');
 const banner = require('@jswork/craco-plugin-banner');
 const analyzer = require('@jswork/craco-plugin-bundle-analyzer');
 const styled = require('@jswork/craco-plugin-styled-components');
-// const debug = require('@jswork/craco-plugin-debug');
+const debug = require('@jswork/craco-plugin-debug');
+
+// 特别提醒: spm 使用的时候，会导致 %PUBLIC_URL%，无法编译。 DllRefs 组件失效等问题。
 
 module.exports = {
   plugins: [
     { plugin: spaBase },
-    { plugin: spm },
+    // { plugin: spm },
     { plugin: banner },
     { plugin: analyzer },
     { plugin: dllRefs },
     { plugin: styled },
+    // { plugin: debug },
   ],
 };
