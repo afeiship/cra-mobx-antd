d316b5b705ac71841e850586e2d6f42635d2e30a
afeiship <1290657123@qq.com>
2022-03-23 19:21:20 +0800

    fix: hmr hacked

--- src/modules/cat-list/index.tsx
+++ src/modules/cat-list/index.tsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const CatList = (props) => (
   <React.Fragment>
-    <h1>Cat list</h1>
+    <h1>Cat2 list</h1>
+    <input type="text" />
     <ul className="list-group">
       {Array.isArray(props.cats) &&
         props.cats.map((cat, i) => (
